62bbce7c9c754fd72ca46ab326b3ebd59e5343f1
Guntur Putra <goldguntur1623@gmail.com>
2024-06-05 12:59:26 +0700

    menambahkan animasi timeline gsap pada section home dan pengurangan bug serta fitur

--- src/components/section/home.jsx
+++ src/components/section/home.jsx
@@ -1,14 +1,109 @@
+import React, { useRef, useEffect, useState } from "react";
 import CardDialoge2 from "../ui/card-dialoge2";
 import { TypeAnimation } from "react-type-animation";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import CardDialoge from "../ui/card-dialoge";
 export default function home() {
+  const title1 = useRef(null);
+  const title2 = useRef(null);
+  const tittle3 = useRef(null);
+  const cardAni = useRef(null);
+  const imageAni = useRef(null);
+  const imageAniRes = useRef(null);
+
+  let newText = `
+  My name's guntur putra hermawan, i was born on 10 november 2007 in
+  indonesia, i started coding in 2023 and when this portfolio was
+  created I had already reached the javascript framework stage of
+  roadmap.sh and that's why this portfolio website was created with React
+  and Tailwind CSS`;
+
+  useEffect(() => {
+    const tl = gsap.timeline({});
+    tl.fromTo(
+      imageAniRes.current,
+      { x: -100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 0.8 }
+    );
+
+    tl.fromTo(
+      title1.current,
+      { x: -100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 0.8 }
+    );
+    tl.fromTo(
+      title2.current,
+      { x: -100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 0.6 }
+    );
+
+    tl.fromTo(
+      tittle3.current,
+      { x: -100, opacity: 0 },
+      { x: 0, opacity: 1, duration: 0.4 }
+    );
+
+    tl.fromTo(
+      ".word",
+      {
+        y: 10,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.3,
+        stagger: 0.1,
+        ease: "power1.inOut",
+      }
+    );
+
+    tl.fromTo(
+      cardAni.current,
+      {
+        y: 15,
+        x: -10,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        duration: 1,
+      }
+    );
+
+    tl.fromTo(
+      imageAni.current,
+      {
+        y: -15,
+        x: 10,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        x: 0,
+        opacity: 1,
+        duration: 1,
+      }
+    );
+
+    return () => {
+      if (tl) tl.kill();
+    };
+  }, []);
   return (
     <section
       id="home"
       className="mx-auto mb-32 px-8 pt-9 lg:grid-flow-col-dense lg:h-[100vh]   lg:grid lg:grid-cols-2 lg:grid-rows-2 lg:place-items-center flex items-center flex-col "
     >
       <article className="lg:w-[45vw]   mb-24 lg:mb-0 text-center lg:text-justify">
-        <div className="relative flex justify-center lg:hidden ">
+        <div
+          className="relative flex justify-center lg:hidden "
+          ref={imageAniRes}
+        >
           <img
             src="/images/blob.svg"
             className="absolute w-[120px] md:w-[180px] -z-50"
@@ -20,13 +115,16 @@ export default function home() {
             className="w-[150px] md:w-[200px] -translate-y-16"
           />
         </div>
-        <h1 className="font-bold lg:text-4xl text-3xl mb-3">
+        <h1
+          ref={title1}
+          className="heading-hero font-bold lg:text-4xl text-3xl mb-3"
+        >
           Introducing <span className="text-primary">myself</span>
         </h1>
-        <h2 className="font-semibold lg:text-3xl text-2xl mb-1">
+        <h2 ref={title2} className="font-semibold lg:text-3xl text-2xl mb-1">
           <span className="text-primary">Hi there,</span> my name is Guntur
         </h2>
-        <h3 className=" font-medium lg:text-2xl text-xl mb-2">
+        <h3 className=" font-medium lg:text-2xl text-xl mb-2" ref={tittle3}>
           I am
           <span className="text-primary">
             <TypeAnimation
@@ -46,25 +144,23 @@ export default function home() {
             />
           </span>
         </h3>
-        <p className="text-xs md:text-sm">
-          My name's guntur putra hermawan, i was born on 10 november 2007 in
-          indonesia, i started coding in 2023 and when this portfolio was
-          created I had already reached the javascript framework stage of
-          <a href="http://roadmap.sh" className=" underline">
-            {" "}
-            roadmap.sh
-          </a>{" "}
-          and that's why this portfolio website was created with React
+
+        <p className="text-xs md:text-sm text-center lg:text-justify" id="text">
+          {newText.split(" ").map((word) => (
+            <span key={word} className={`word inline-block ml-0 mr-1 t`}>
+              {word}
+            </span>
+          ))}
         </p>
       </article>
-      <section className="lg:w-[45vw] lg:mx-0 mx-auto">
+      <section className="lg:w-[45vw] lg:mx-0 mx-auto" ref={cardAni}>
         <CardDialoge2
           image="/Certificate/js-intermediate.png"
           className="-z-50 "
         />
       </section>
 
-      <aside className="row-span-2 hidden lg:block">
+      <aside className="row-span-2 hidden lg:block" ref={imageAni}>
         <img
           src="/images/guntur-parka.png"
           alt="guntur"
--- src/components/section/navbar.jsx
+++ src/components/section/navbar.jsx
@@ -1,39 +1,105 @@
-export default function Navbar({ name, logoImg, alt }) {
-  return (
-    <nav className="navbar sticky lg:fixed top-0 block z-50 h-max w-full max-w-full">
-      <div className=" flex bg-black  justify-between  items-center  p-1 lg:p-3">
-        <h1 className=" text-primary text-xl ">
-          {name}e<span className=" text-white">17</span>
-        </h1>
-        <ul className="flex gap-4 justify-center items-center ">
-          <li>
-            <a href="#home" className="text-primary">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#about" className="text-primary">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#projects" className="text-primary">
-              Projects
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="text-primary">
-              Contact
-            </a>
-          </li>
-        </ul>
-        {/* icon img if click to dry text*/}
-        <div className="relative">
-          <img src={logoImg} alt={alt} className="rounded-full w-9 peer" />
+import { useState, useEffect } from "react";
 
-          <p className="p-2 text-xs opacity-0 -translate-y-3 peer peer-hover:-translate-y-0 peer-hover:opacity-100 absolute transition bg-slate-600 rounded-md text-nowrap right-1 text-white">
-            {name}
-          </p>
+export default function Navbar({ name }) {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const [isMdScreen, setIsMdScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMdScreen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function toggleMenu() {
+    setIsMenuVisible(!isMenuVisible);
+  }
+  return (
+    <nav className="navbar fixed  top-0 block z-50  w-full ">
+      <div className=" flex  relative bg-black  justify-between  lg:items-center  p-1 lg:p-3">
+        <h1 className="top-0  text-primary text-xl ">
+          {name}e<span className=" text-white ">17</span>
+        </h1>
+        <div
+          className={`w-full ${
+            isMenuVisible
+              ? "-translate-y-0 md:transition-none"
+              : "-translate-y-full md:-translate-y-0"
+          }  transition md:transition-none linear w-full  uppercase md:flex justify-center md:static" id="list-default`}
+        >
+          <ul
+            className={`flex ${
+              isMenuVisible || isMdScreen
+                ? "-translate-y-0"
+                : "-translate-y-full md:-translate-y-0 hidden "
+            } linear transition md:transition-none w-full  uppercase md:capitalize  gap-x-8 gap-y-12 rounded-md relative  md:static justify-center text-center h-[40vh] md:h-auto items-center md:mt-0  flex-col   md:flex-row ${
+              isMenuVisible && "md:static"
+            } md:text-sm lg:text-base`}
+          >
+            <li>
+              <a
+                href="#home"
+                className="text-primary text-center absolute md:static "
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about"
+                className="text-primary text-center absolute  md:static"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#projects"
+                className="text-primary text-center absolute md:static"
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact"
+                className="text-primary text-center absolute md:static"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex gap-x-2">
+          <button className="bg-primary m-0 top-0 max-h-24 w-24 font-medium h-8 text-sm md:text-base text-nowrap rounded">
+            hire me
+          </button>
+          <button
+            className="top-0 max-h-24 m-0 h-8 rounded md:hidden"
+            onClick={toggleMenu}
+          >
+            <svg
+              class="w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </nav>
