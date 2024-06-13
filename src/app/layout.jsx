a65629224f527f266dcc976d2c9d7d4e43ed7eb6
Guntur Putra <goldguntur1623@gmail.com>
2024-06-13 19:57:49 +0700

    fix the problem bug in mobile design

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -13,6 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
       <body
         className="w-[100vw] max-w-full overflow-x-hidden bg-black"
         suppressHydrationWarning={true}
