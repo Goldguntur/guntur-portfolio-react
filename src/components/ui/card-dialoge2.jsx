dfd400099a42b16c85cfbb7e8395771c8c16bdda
Guntur Putra <goldguntur1623@gmail.com>
2024-06-13 20:31:47 +0700

    menambahkan fitur close

--- src/components/ui/card-dialoge2.jsx
+++ src/components/ui/card-dialoge2.jsx
@@ -115,6 +115,14 @@ export default function CardDialoge2({ image }) {
                       </DisclosurePanel>
                     </Disclosure>
                   </div>
+                  <div className="mt-4">
+                    <Button
+                      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
+                      onClick={close}
+                    >
+                      X
+                    </Button>
+                  </div>
                 </DialogPanel>
               </TransitionChild>
             </div>
